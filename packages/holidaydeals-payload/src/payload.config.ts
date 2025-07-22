35207abbca4139fc28c47329e4a291f7edacb42d
Anil Puri <anilgoswami@startbititsolutions.com>
2025-07-22 12:49:07 +0530

    all commi

--- packages/holidaydeals-payload/src/payload.config.ts
+++ packages/holidaydeals-payload/src/payload.config.ts
@@ -6,24 +6,12 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
-import dotenv from 'dotenv';
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
-import { Blog } from './collections/Blogs'
-import { Tours } from './collections/Tours'
-import { Destination } from './collections/Destination'
-
-import { Booking } from './collections/Booking'
-import { Review } from './collections/Review'
-
-
-
-import toursPaginatedHandler from './payload/custom-endpoints/tours-paginated';
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
-dotenv.config();
 
 export default buildConfig({
   admin: {
@@ -32,14 +20,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  endpoints: [
-    {
-      path: '/tours-paginated',
-      method: 'get',
-      handler: toursPaginatedHandler,
-    },
-  ],
-  collections: [Users, Media, Blog, Tours, Destination, Review, Booking],
+  collections: [Users, Media],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
@@ -55,5 +36,4 @@ export default buildConfig({
     payloadCloudPlugin(),
     // storage-adapter-placeholder
   ],
-
 })
