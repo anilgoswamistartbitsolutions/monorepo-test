5a9d8aceee906737a1db81e83d396907a716e619
Anil Puri <anilgoswami@startbititsolutions.com>
2025-07-23 16:29:43 +0530

    Update Users.ts

--- packages/holidaydeals-payload/src/collections/Users.ts
+++ packages/holidaydeals-payload/src/collections/Users.ts
@@ -2,12 +2,39 @@ import type { CollectionConfig } from 'payload'
 
 export const Users: CollectionConfig = {
   slug: 'users',
-  admin: {
-    useAsTitle: 'email',
-  },
   auth: true,
+  admin: {
+    useAsTitle: 'name',
+  },
+  access: {
+    read: () => true,
+  },
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      label: 'Full Name',
+      name: 'name',
+      type: 'text',
+      required: true,
+      defaultValue: '',
+      // removed admin.hidden so it shows
+    },
+
+    {
+      name: 'profileImage',
+      type: 'upload',
+      relationTo: 'media',
+      label: 'Profile Image',
+      admin: {
+        condition: (data, siblingData, { user }) => Boolean(user),
+      },
+      // removed admin.hidden
+    },
+    {
+      name: 'isAuthor',
+      type: 'checkbox',
+      label: 'Are you a author?',
+      defaultValue: false,
+      // removed admin.hidden
+    },
   ],
 }
